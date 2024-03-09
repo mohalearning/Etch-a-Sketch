1504123d653610275877ea3638ab2bc42342c1bc
mohalearning <mohalearning01@gmail.com>
2024-03-09 09:37:33 +0100

    Feat: The game and UI functioning correctely

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ let color = false;
 
 function createGrid(dimension) {
     let square = 400.0 / parseFloat(dimension);
-    console.log(square);
     for (let i = 0; i < dimension; i++) {
         for (let j = 0; j < dimension; j++) {
             const grid = document.createElement('div');
             grid.margin = 0;
             grid.padding = 0;
-            grid.style = `border: 1px solid red;width:${square.toString()}px;height:${square.toString()}px;`;
+            grid.style = `border: 1px solid black;width:${square.toString() -2}px;height:${square.toString() -2}px;`;
             grid.id = i.toString() + "-" + j.toString() + "-id";
             container.appendChild(grid);
         }
@@ -33,16 +32,18 @@ function getRandomRgbColor() {
 
 document.addEventListener('mousemove', function (e) {
     console.log(e.target.localName);
-    if (e.target.localName == "div" && e.target.id != "box" && e.target.id != "container") {
+    if (e.target.localName == "div" &&
+        e.target.id != "box" &&
+        e.target.id != "container" &&
+        e.target.id != "buttons") {
         if (color) {
             let randomRGBColor = getRandomRgbColor();
             document.getElementById(e.target.id).style.backgroundColor = `rgb(${randomRGBColor[0]}, ${randomRGBColor[1]}, ${randomRGBColor[2]})`;
         }
         else
-            document.getElementById(e.target.id).style.backgroundColor = "red";
+            document.getElementById(e.target.id).style.backgroundColor = "lightblue";
     }
 });
-
 
 function chooseGrid() {
     const gridNumber = prompt("Chose the number of grid side that you want from 2 to 100?");
